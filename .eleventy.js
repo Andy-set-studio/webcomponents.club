57d693dc492894bba19a8cbd1ac3c58f099e68d6
Andy Bell <me@andy-bell.co.uk>
2018-07-26 11:24:47 +0100

    Add RSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,32 @@
 const dateFilter = require('./filters/date-filter.js');
+const htmlmin = require('html-minifier');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const videoPosterShortCode = require('./shortcodes/video-poster-shortcode.js');
 
 module.exports = function(eleventyConfig) { 
+    
+    // Plugins
+    eleventyConfig.addPlugin(rssPlugin);
+
     // Add shortcodes
     eleventyConfig.addShortcode('video_poster', videoPosterShortCode);
     
     // Filters 
     eleventyConfig.addFilter('date', dateFilter);
-
+    
+    // Minify
+    eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+        if(outputPath.indexOf('.html') > -1) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                minifyCSS: true
+            });
+            return minified;
+        }
+        return content;
+    });
     // Return config settings
     return {
         markdownTemplateEngine: 'njk'
